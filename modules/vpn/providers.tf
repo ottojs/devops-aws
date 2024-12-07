f073f5457e19f871ade85411da25a4855aae451e
Ryan Lelek <ryanlelek@gmail.com>
2024-12-07 17:10:42 -0600

    Dependencies: Upgrading provider aws from v5.76.0 to v5.80.0

--- modules/vpn/providers.tf
+++ modules/vpn/providers.tf
@@ -5,7 +5,7 @@ terraform {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "5.76.0"
+      version = "5.80.0"
     }
   }
 }
