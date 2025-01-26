110f699c1b761e16cdea0ce19a1fdbdbbf4e594f
Ryan Lelek <ryanlelek@gmail.com>
2025-01-26 12:03:24 -0600

    Dependencies: Upgrading provider aws from v5.83.1 to v5.84.0

--- aws_account/01_tf_backend/providers.tf
+++ aws_account/01_tf_backend/providers.tf
@@ -11,7 +11,7 @@ terraform {
     # https://registry.terraform.io/providers/hashicorp/aws/latest
     aws = {
       source  = "hashicorp/aws"
-      version = "5.83.1"
+      version = "5.84.0"
     }
   }
 }
