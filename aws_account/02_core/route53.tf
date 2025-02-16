6a3bbf4bada62608b64ac68d8614b39b6375c7fd
Ryan Lelek <ryanlelek@gmail.com>
2025-02-16 14:18:57 -0600

    Remove: Route 53 subdomain setup

--- aws_account/02_core/route53.tf
+++ aws_account/02_core/route53.tf
@@ -18,34 +18,6 @@ resource "aws_route53_zone" "root_private" {
   })
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_zone
-resource "aws_route53_zone" "dev_public" {
-  name = "dev.${var.root_domain}"
-  tags = merge(var.tags, {
-    Public = "true"
-  })
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_zone
-resource "aws_route53_zone" "dev_private" {
-  name = "dev.${var.root_domain}."
-  vpc {
-    vpc_id = module.myvpc.vpc.id
-  }
-  tags = merge(var.tags, {
-    Public = "false"
-  })
-}
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
-resource "aws_route53_record" "ns_dev" {
-  zone_id = aws_route53_zone.root_public.zone_id
-  name    = "dev.${var.root_domain}"
-  type    = "NS"
-  ttl     = "3600"
-  records = aws_route53_zone.dev_public.name_servers
-}
-
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
 resource "aws_route53_record" "root_public_caa" {
   zone_id = aws_route53_zone.root_public.zone_id
