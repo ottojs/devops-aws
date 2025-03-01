169833e12b5afa705c0f77cdbb4271b204ab321c
Ryan Lelek <ryanlelek@gmail.com>
2025-03-01 17:04:36 -0600

    Add: Wildcard certificate in AWS Certificate Manager

--- modules/route53_root/main.tf
+++ modules/route53_root/main.tf
@@ -31,3 +31,30 @@ resource "aws_route53_record" "root_public_caa" {
     "0 issue \"letsencrypt.org\""
   ]
 }
+
+############################
+### Wildcard Certificate ###
+############################
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.${var.root_domain}"
+  validation_method = "DNS"
+  tags              = var.tags
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
+resource "aws_route53_record" "root_public_acm" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  zone_id = aws_route53_zone.root_public.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
