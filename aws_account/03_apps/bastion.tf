8f7a4c1dd599e81aea392069e9db8b78f6bbc7b6
Ryan Lelek <ryanlelek@gmail.com>
2025-04-24 16:45:57 -0500

    Add: Bastion machine - Debian 12

--- aws_account/03_apps/bastion.tf
+++ aws_account/03_apps/bastion.tf
@@ -8,11 +8,19 @@ data "aws_iam_instance_profile" "ec2" {
   name = "ec2-ssm-profile"
 }
 
+# Important Notes:
+#
 # EC2 Machine - Amazon Linux 2023 (RedHat-based, x86_64)
 # If you don't want the underlying os to rebuild you
 # need to pin the AMI version with the following:
 # ami = "ami-xxxxxxxxxxxxxxxxx"
-module "bastion_x86_64" {
+#
+# If you need to attach an SSH Key (not recommended, except for debugging)
+# Then add the following line:
+# ssh_key = "name-of-key"
+
+# Bastion - AL2023 x86_64
+module "bastion_al2023_x86_64" {
   source               = "../../modules/ec2"
   name                 = "bastion"
   subnet_id            = data.aws_subnets.private.ids[0]
@@ -26,11 +34,8 @@ module "bastion_x86_64" {
   tags                 = var.tags
 }
 
-# EC2 Machine - Amazon Linux 2023 (RedHat-based, ARM64)
-# If you don't want the underlying os to rebuild you
-# need to pin the AMI version with the following:
-# ami = "ami-xxxxxxxxxxxxxxxxx"
-module "bastion_arm64" {
+# Bastion - AL2023 ARM64
+module "bastion_al2023_arm64" {
   source               = "../../modules/ec2"
   name                 = "bastion"
   subnet_id            = data.aws_subnets.private.ids[0]
@@ -40,6 +45,22 @@ module "bastion_arm64" {
   security_groups      = [data.aws_security_group.main.id]
   iam_instance_profile = data.aws_iam_instance_profile.ec2
   userdata             = "../../userdata/rhel.sh"
+  kms_key              = data.aws_kms_key.main
+  tags                 = var.tags
+}
+
+# Bastion - Debian 12 Bookworm
+# You can also use Debian 11 Bullseye
+module "bastion_debian_x86_64" {
+  source               = "../../modules/ec2"
+  name                 = "bastion"
+  subnet_id            = data.aws_subnets.private.ids[0]
+  os                   = "debian12"
+  arch                 = "x86_64"
+  machine              = "t3a.small"
+  security_groups      = [data.aws_security_group.main.id]
+  iam_instance_profile = data.aws_iam_instance_profile.ec2
+  userdata             = "../../userdata/debian.sh"
   kms_key              = data.aws_kms_key.main
   tags                 = var.tags
 }
